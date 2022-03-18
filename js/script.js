57304ef702c9b77c2ff1ec35e4e721cb7930e462
joaopsilv <joao_p_silveira@estudante.sc.senai.br>
2022-03-18 13:52:38 -0300

    password reveal added - 18/03

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-AOS.init();
+/* Password Reveal */
+let inputPassword = document.getElementById("inputPassword")
+
+let passwordState = document.getElementsByClassName("password-state")[0]
+let iconP = document.getElementById("iconPassword")
+
+passwordState.addEventListener("click", ()=>{
+    iconP.classList.toggle("fa-eye-slash")
+    iconP.classList.toggle("fa-eye")
+    
+    if(inputPassword.type == "password"){
+        inputPassword.type = "text"
+    }
+    else{
+        inputPassword.type = "password"
+    }
+})
+
+/* PasswordConfirm Reveal */
+let inputPasswordConfirm = document.getElementById("inputPasswordConfirm")
+
+let passwordConfirmState = document.getElementsByClassName("passwordconfirm-state")[0]
+let iconPC = document.getElementById("iconPasswordConfirm")
+
+passwordConfirmState.addEventListener("click", ()=>{
+    iconPC.classList.toggle("fa-eye-slash")
+    iconPC.classList.toggle("fa-eye")
+    
+    if(inputPasswordConfirm.type == "password"){
+        inputPasswordConfirm.type = "text"
+    }
+    else{
+        inputPasswordConfirm.type = "password"
+    }
+})
